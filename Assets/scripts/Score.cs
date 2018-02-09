ea78f5f82cabf60ae49ec4fd3bd668d1b5877a9c
CharlieLeee <lichengkun0805@gmail.com>
2018-02-09 14:18:24 +0800

    stop scoring if past last obstacle

--- Assets/scripts/Score.cs
+++ Assets/scripts/Score.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour {
 
     public Transform player;
+    public Transform lastObj;
     public Text scoreText;
 	
 	// Update is called once per frame
 	void Update () {
-        if (!FindObjectOfType<GameManager>().gameHasEnded)
+        if (!FindObjectOfType<GameManager>().gameHasEnded && !(player.position.z > lastObj.position.z))
             scoreText.text = player.position.z.ToString("0");
     }
 }
